3f78f612fd40dd36693e65594d298cb4640b6653
JULES123e <elliot.fesquet@efrei.net>
2025-05-20 11:06:39 +0200

    init commit

---
+++ src/config.mjs
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://test:test@cluster0.erx83.mongodb.net/'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://test:test@cluster0.erx83.mongodb.net/'
+  }
+};
